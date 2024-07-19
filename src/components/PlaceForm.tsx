122ae872e63cc66ab6e2cb141b9c6fbbc085c8e1
Diogo-NB <diogonunes.20022@gmail.com>
2024-07-19 03:13:25 -0300

    Add placeTitle TextInput

--- src/components/PlaceForm.tsx
+++ src/components/PlaceForm.tsx
@@ -1,5 +1,29 @@
-import { Text } from "react-native-paper";
+import { useCallback, useState } from "react";
+import { ScrollView, StyleSheet } from "react-native";
+import { Text, TextInput } from "react-native-paper";
 
 export default function PlaceForm() {
-    return <Text variant="headlineMedium" >The Place Form</Text>
+  const [placeTitle, setPlaceTitle] = useState("");
+
+  const changePlaceTitleHandler = useCallback((text: string) => {
+    setPlaceTitle(text);
+  }, []);
+
+  return (
+    <ScrollView style={styles.formContainer}>
+      <TextInput
+        mode="outlined"
+        onChangeText={changePlaceTitleHandler}
+        value={placeTitle}
+        label={"Place Title"}
+      />
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  formContainer: {
+    flex: 1,
+    padding: 24,
+  },
+});
