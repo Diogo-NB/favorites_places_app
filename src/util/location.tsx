e776d59c42eabf0d3c8e396d5d6da12436639bb0
Diogo-NB <diogonunes.20022@gmail.com>
2024-07-20 02:19:07 -0300

    Fix .env for API key & add .env example

--- src/util/location.tsx
+++ src/util/location.tsx
@@ -2,17 +2,17 @@ import { LatLng } from "react-native-maps";
 
 export type LocationData = LatLng & { address: string };
 
+const GOOGLE_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_API_KEY ?? '';
 
 export function getMapPreviewUrl({ latitude, longitude }: LatLng) {
-  const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
   const url = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=14&size=400x200&maptype=roadmap
 &markers=color:red%7Clabel:S%7C${latitude},${longitude}
 &key=${GOOGLE_API_KEY}`;
+
   return url;
 }
 
 export async function getAdress({ latitude, longitude }: LatLng) {
-  const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
   const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${GOOGLE_API_KEY}`;
   const res = await fetch(url);
 
